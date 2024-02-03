eb1d3c82a760fda6c93fda61899e005bf4f9b22a
ErickLima <ericklimacosta29@gmail.com>
2024-02-03 13:28:03 -0300

    Controler user

---
+++ src/user/user.controller.ts
@@ -0,0 +1,26 @@
+// eslint-disable-next-line prettier/prettier
+import { Controller, Get, Post, Body, Query } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+// import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Get('/email')
+  async findByEmail(@Query('email') email: string) {
+    console.log(email);
+    return this.userService.findByEmail(email);
+  }
+
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  //   return this.userService.update(+id, updateUserDto);
+  // }
+}
