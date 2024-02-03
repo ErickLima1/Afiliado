b5eca0129d33fd8ae485c0bc9952c3b44b1da85a
ErickLima <ericklimacosta29@gmail.com>
2024-02-03 13:30:39 -0300

    Service User

---
+++ src/user/user.service.ts
@@ -0,0 +1,46 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+// import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const existingUser = await this.prisma.usuarioAdm.findUnique({
+      where: {
+        email: createUserDto.email,
+      },
+    });
+
+    if (existingUser) {
+      // console.log('Caiu Aqui', existingUser);
+      throw new HttpException('Email Já Em Uso', HttpStatus.NOT_FOUND);
+    }
+
+    const data = {
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 10),
+    };
+
+    const createdUser = await this.prisma.usuarioAdm.create({ data });
+
+    return {
+      ...createdUser,
+      password: undefined,
+    };
+  }
+
+  findByEmail(email: string) {
+    console.log(email);
+    return this.prisma.usuarioAdm.findUnique({
+      where: { email },
+    });
+  }
+
+  // update(id: number, updateUserDto: UpdateUserDto) {
+  //   return `This action updates a #${id} user`;
+  // }
+}
