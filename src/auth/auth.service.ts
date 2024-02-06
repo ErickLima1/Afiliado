e57350bdb5b0ddca6953e79e8b6809bf14dc039c
ErickLima <ericklimacosta29@gmail.com>
2024-02-06 17:00:41 -0300

    auth

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
+import { User } from 'src/user/entities/user.entity';
+import { UserPayload } from './models/UserPayload';
+import { JwtService } from '@nestjs/jwt';
+import { UserToken } from './models/UserToken';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  login(user: User): UserToken {
+    // Vai trasnformar o user em JWT
+    const payload: UserPayload = {
+      sub: user.id,
+      email: user.email,
+      nome: user.nome,
+    };
+    const jwtToken = this.jwtService.sign(payload);
+
+    return {
+      access_token: jwtToken,
+    };
+  }
+
+  async validateUser(email: string, password: string) {
+    const user = await this.userService.findByEmail(email);
+
+    if (user) {
+      //Checando Senha
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      if (isPasswordValid) {
+        return {
+          ...user,
+          password: undefined,
+        };
+      }
+    }
+    //Se Chegou Aqui, Significa que não encontrou um user ou a Senha esta incorreta.
+    throw new Error('Email ou Senha Esta incorreta. Tente Novamente !');
+  }
+}
